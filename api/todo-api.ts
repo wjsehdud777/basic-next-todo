c83500873729c0583620636ad2ddd20da5d7dd8a
JDY <wjsehdud@nate.com>
2025-03-20 19:33:26 +0900

    20회차: 할 일 목록 API에 재검증 기능 추가: fetch 요청에 태그 추가 및 TodoItem 컴포넌트에서 삭제 및 완료 토글 기능 구현

--- api/todo-api.ts
+++ api/todo-api.ts
@@ -1,9 +1,16 @@
+"use server";
+
 import { Todo } from "@/types/todo.type";
+import { revalidateTag } from "next/cache";
 
 const BASE_URL = "http://localhost:3000/todos";
 
 export const getTodos = async () => {
-  const response = await fetch(BASE_URL);
+  const response = await fetch(BASE_URL, {
+    next: {
+      tags: ["todos"],
+    },
+  });
   const data: Todo[] = await response.json();
 
   return data;
@@ -30,6 +37,8 @@ export const deleteTodo = async (id: Todo["id"]) => {
 
   const data: Todo = await response.json();
 
+  revalidateTag("todos");
+
   return data;
 };
 
@@ -45,6 +54,8 @@ export const toggleTodoCompleted = async (
     body: JSON.stringify({ completed }),
   });
   const data: Todo = await response.json();
+
+  revalidateTag("todos");
 
   return data;
 };
