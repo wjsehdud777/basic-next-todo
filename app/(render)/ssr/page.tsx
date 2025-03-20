a4088d6b5e9fb00131380ba4ed5099ee100f617e
JDY <wjsehdud@nate.com>
2025-03-20 16:42:38 +0900

    19회차: SSR 페이지 추가 및 API를 통해 할 일 목록 데이터를 가져오는 기능 구현

---
+++ app/(render)/ssr/page.tsx
@@ -0,0 +1,11 @@
+import { Todo } from "@/types/todo.type";
+import React from "react";
+
+const SSRPage = async () => {
+  const response = await fetch("http://localhost:3000/todos");
+  const data: Todo[] = await response.json();
+
+  return <div>SSRPage: {JSON.stringify(data)}</div>;
+};
+
+export default SSRPage;
