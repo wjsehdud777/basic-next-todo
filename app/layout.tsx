7ba109d8f253bde79d0702f4c1837a8e2d23aa04
JDY <wjsehdud@nate.com>
2025-03-21 10:10:04 +0900

    21회차: TodoHeader 컴포넌트 생성 및 스타일링 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TodoHeader from "@/components/todo/layout/TodoHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TodoHeader />
         {children}
       </body>
     </html>
---
+++ components/todo/layout/TodoHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoHeader = () => {
+  return (
+    <header className="mb-4">
+      <div className="container mx-auto p-2">
+        <h1 className="text-4xl font-bold">Todo List</h1>
+      </div>
+    </header>
+  );
+};
+
+export default TodoHeader;
