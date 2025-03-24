95403ec98353d0f242cc0fc78f5c4f9dcfee0040
JDY <wjsehdud@nate.com>
2025-03-24 09:38:06 +0900

    21회차: 상세 페이지에 TodoItem을 감싸는 컨테이너 추가 및 돌아가기 버튼 링크 추가

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { getTodoItem } from "@/api/todo-api";
 import TodoItem from "@/components/todo/TodoItem";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 interface DetailPageProps {
@@ -13,7 +15,13 @@ const DetailPage = async ({ params }: DetailPageProps) => {
 
   return (
     <section>
-      <TodoItem todo={todoItem} />
+      <div className="container p-2 mx-auto space-y-4">
+        <TodoItem todo={todoItem} />
+      </div>
+
+      <Link href="/">
+        <Button className="w-full">돌아가기</Button>
+      </Link>
     </section>
   );
 };
