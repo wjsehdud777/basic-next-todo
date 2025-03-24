c31d30389c9be9b4ab6e81c881c54d13f035549b
JDY <wjsehdud@nate.com>
2025-03-24 09:40:09 +0900

    21회차: TodoHeader 컴포넌트에 링크 추가하여 제목 클릭 시 홈으로 이동하도록 수정

--- components/todo/layout/TodoHeader.tsx
+++ components/todo/layout/TodoHeader.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const TodoHeader = () => {
   return (
     <header className="mb-4">
-      <div className="container mx-auto p-2">
-        <h1 className="text-4xl font-bold">Todo List</h1>
+      <div className="container mx-auto p-2 flex flex-row">
+        <Link href="/">
+          <h1 className="text-4xl font-bold">Todo List</h1>
+        </Link>
       </div>
     </header>
   );
