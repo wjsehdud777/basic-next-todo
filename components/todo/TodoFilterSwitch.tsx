87649a161643b3b83af1459225cebb9c734f0b49
JDY <wjsehdud@nate.com>
2025-03-25 20:20:35 +0900

    23회차: Todo 필터 스위치 컴포넌트에 상태 관리 추가 및 체크박스 동작 구현

--- components/todo/TodoFilterSwitch.tsx
+++ components/todo/TodoFilterSwitch.tsx
@@ -3,13 +3,23 @@
 import { useId } from "react";
 import { Label } from "../ui/label";
 import { Switch } from "../ui/switch";
+import { useTodoFilterStore } from "@/store/useTodoFilterStore";
 
 const TodoFilterSwitch = () => {
   const switchId = useId();
+  const { filter, setFilter } = useTodoFilterStore();
+
+  const onCheckedChange = (checked: boolean) => {
+    setFilter(checked ? "completed" : "all");
+  };
 
   return (
     <div className="flex items-center space-x-2">
-      <Switch id={switchId} />
+      <Switch
+        id={switchId}
+        checked={filter === "completed"}
+        onCheckedChange={onCheckedChange}
+      />
       <Label htmlFor={switchId}>Completed</Label>
     </div>
   );
