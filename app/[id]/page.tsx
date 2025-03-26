da4c7402d077267392c90f169cd6257545460d3d
JDY <wjsehdud@nate.com>
2025-03-26 22:08:41 +0900

    24회차: Supabase 클라이언트를 사용하여 Todo 항목 상세 조회 기능 개선

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,7 @@
+import { getTodoItem } from "@/api/todo-api";
 import TodoDetail from "@/components/todo/TodoDetail";
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/server";
 import {
   dehydrate,
   HydrationBoundary,
@@ -14,17 +16,19 @@ interface DetailPageProps {
 const DetailPage = async ({ params }: DetailPageProps) => {
   const { id } = await params;
   const queryClient = new QueryClient();
+  const supabaseClient = await createClient();
+  const todoId = Number(id);
 
-  // await queryClient.prefetchQuery({
-  //   queryKey: ["todos", id],
-  //   queryFn: () => getTodoItem(id),
-  // });
+  await queryClient.prefetchQuery({
+    queryKey: ["todos", todoId],
+    queryFn: () => getTodoItem(supabaseClient, todoId),
+  });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <section>
         <div className="container p-2 mx-auto space-y-4">
-          <TodoDetail id={Number(id)} />
+          <TodoDetail id={todoId} />
 
           <Link href="/">
             <Button className="w-full">돌아가기</Button>
